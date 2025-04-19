c47203a98b95c966cc95bb0e6c9fddc19968426e
ITHwang <hwangintech@gmail.com>
2025-04-20 02:11:14 +0900

    feat: add a gradient descent example

---
+++ gradient_descent.py
@@ -0,0 +1,29 @@
+import torch
+from torch import Tensor
+
+
+def rosenbrock(x0: torch.Tensor, x1: torch.Tensor) -> torch.Tensor:
+    y = 100 * (x1 - x0**2) ** 2 + (x0 - 1) ** 2
+
+    assert isinstance(y, torch.Tensor)
+
+    return y
+
+
+if __name__ == "__main__":
+    x0 = torch.tensor(0.0, requires_grad=True)
+    x1 = torch.tensor(2.0, requires_grad=True)
+    lr = 0.001
+    iters = 1000
+
+    for i in range(iters):
+        print(x0, x1)
+
+        y = rosenbrock(x0, x1)
+
+        x0.cleargrad()
+        x1.cleargrad()
+        y.backward()
+
+        x0.data -= lr * x0.grad  # type: ignore
+        x1.data -= lr * x1.grad  # type: ignore
